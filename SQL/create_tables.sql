c60570e46bb00d91a894085f6371ab1b2a8fcba5
subarnakirtaniya <subarnakirtaniya926@gmail.com>
2026-04-03 00:24:37 +0530

    Create SQL/create_tables.sql
    SQL table creation

---
+++ SQL/create_tables.sql
@@ -0,0 +1,14 @@
+CREATE TABLE daily_sales (
+order_date DATE,
+daily_sales FLOAT
+);
+
+CREATE TABLE product_sales (
+product TEXT,
+product_sales FLOAT
+);
+
+CREATE TABLE customer_sales (
+customer_id INT,
+customer_sales FLOAT
+);
